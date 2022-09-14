dbfe6ca206f231f3a29b6bea0d58fe45c0f847d7
Jacobojijo <jacobojijo2017@gmail.com>
2022-09-14 07:12:35 -0700

    slight

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-
 /**
- * main - key function
- *
- * Return: Always 0 success
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
  */
-
 int main(void)
 {
-	int n;
+	int c = 0;
 
-	while (n < 10)
+	while (c < 10)
 	{
-		printf("%d", n);
-		n++;
+		putchar(48 + c);
+		c++;
 	}
+
 	putchar('\n');
 	return (0);
 }
