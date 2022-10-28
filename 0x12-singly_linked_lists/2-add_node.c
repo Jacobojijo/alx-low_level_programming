2256af22887c3a195535c10829c572285a7388eb
Jacobojijo <jacobojijo2017@gmail.com>
2022-10-27 22:49:21 -0700

    Third task

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,37 +1,41 @@
 #include "lists.h"
-#include <string.h>
 
 /**
- * add_node - Adds a new node at the beginning
- *            of a list_t list.
- * @head: A pointer to the head of the list_t list.
- * @str: The string to be added to the list_t list.
- *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new element.
+ * _strlen - counts the lenght of a string
+ * @str: string to be counted
+ * Return: len of a string
  */
+
+int _strlen(const char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node - adds a node at the beginning
+ * @head: first parameter
+ * @str: second parameter
+ * Return: nodes
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	char *dup_str;
-	int len;
 
 	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
 
-	dup_str = strdup(str);
-	if (dup_str == NULL)
+	if (new == NULL)
 	{
-		free(new);
 		return (NULL);
 	}
-
-	for (len = 0; str[len];)
-		len++;
-
-	new->str = dup_str;
-	new->len = len;
+	new->str = strdup(str);
+	new->len = _strlen(str);
 	new->next = *head;
 
 	*head = new;
